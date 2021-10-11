a0674bec5e976a36fd6532e6be3b240378cb1fd4
rinsolao <olimeed@gmail.com>
2021-10-11 12:43:09 +0100

    Updated Fonts, Changed Some Colors, Add Text To Header

---
+++ src/components/Contact/ContactHeader/ContactHeader.js
@@ -0,0 +1,13 @@
+import "./ContactHeader.css";
+import Header from "../../Header/Header";
+
+const ContactHeader = () => {
+  return (
+    <div className="contact-container container">
+      <Header />
+      <h5>Contact Us</h5>
+    </div>
+  );
+};
+
+export default ContactHeader;
